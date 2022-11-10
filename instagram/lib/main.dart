b7c4e2739aad564b88fc27dbe37e154225b55e20
sangyeob <rlatkdduq99@gmail.com>
2022-11-10 22:04:00 +0900

    feat : GalleryImage/shared preferences/gestureDetector/Provider

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -1,20 +1,28 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'style/appbarStyle.dart' as appbarstyle;
 import 'widget/BottomWidget.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter/rendering.dart'; //스크롤 높이와 관련 함수 사용가능
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      theme : appbarstyle.appbartheme, 
-      initialRoute: '/',
-      // routes: {
-      //   '/' : (c) => Text('첫페이지'),
-      //   '/detail' : (c) => Text('둘째페이지')
-      // },
-      home : MyApp()
+    ChangeNotifierProvider(
+      create: (BuildContext context) => StateStore(),
+      child: MaterialApp(
+        theme : appbarstyle.appbartheme, 
+        initialRoute: '/',
+        // routes: {
+        //   '/' : (c) => Text('첫페이지'),
+        //   '/detail' : (c) => Text('둘째페이지')
+        // },
+        home : MyApp()
+      ),
     )
   );
 }
@@ -31,7 +39,21 @@ class _MyAppState extends State<MyApp> {
   var tab = 0;
   var scrolldown = false;
   var data = [];
+  var userImage;  
+  var userContent;
   
+  saveData() async {
+
+    //서버로부터 가져온 걸 내장메모리에 저장해두면, 더 빠르게 업로드를 할 수 있다.
+    var storage = await SharedPreferences.getInstance();
+    // storage.setString('name', 'john');  
+    // var result = storage.getString('name');
+    var map = {'statuscode' : 200};
+    storage.setString('map', jsonEncode(map));
+    var result = storage.getString('map') ?? 'Nope';
+    print(jsonDecode(result)['statuscode']);
+  }
+
   getData() async {
     var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
     if(result.statusCode == 200){
@@ -47,8 +69,32 @@ class _MyAppState extends State<MyApp> {
     });  
   }
 
+  addmydata(){
+    var mydata = {
+      'id' : data.length,
+      'image' : userImage,
+      'likes' : 0,
+      'data' : 'July 25',
+      'content' : userContent,
+      'liked' : false,
+      'user' : 'sangyeob'
+    };
+    print(mydata);
+    setState(() {
+      data.insert(0, mydata);
+    });
+
+  }
+
+  setUserContent(text){
+    setState(() => {
+      userContent = text
+    }); 
+  }
+
   void initState(){
     super.initState();
+    saveData();
     getData();
   }
 
@@ -100,16 +146,23 @@ class _MyAppState extends State<MyApp> {
         title: Text("Instagram"),
         actions : [IconButton(
           icon: Icon(Icons.add_box_outlined),
-          onPressed: (){
+          onPressed: () async{
+            var picker = ImagePicker();
+            var image = await picker.pickImage(source: ImageSource.gallery);
+            if(image !=  null){
+              setState(() {
+                userImage = File(image.path);
+              });
+            }            
             Navigator.push(context, 
-              MaterialPageRoute(builder :(c) => Upload())
+              MaterialPageRoute(builder :(c) => Upload(userImage : userImage, data : data, setUserContent: setUserContent, addmydata: addmydata))
             );
           },
           iconSize: 30)
         ]
       ),
       body : 
-        [Home(data: data, scrollDown : scrollDown, scrollUp: scrollUp), Text('샵페이지')][tab],
+        [Home(data: data, scrollDown : scrollDown, scrollUp: scrollUp, setUserContent : setUserContent), Text('샵페이지')][tab],
       // Column(
       //   children: [
       //     [Text('홈페이지'), Text('샵페이지')][tab],
@@ -131,12 +184,14 @@ class Home extends StatefulWidget {
     Key? key,
     required this.data,
     required this.scrollDown,
-    required this.scrollUp,
+    required this.scrollUp, 
+    required this.setUserContent,
   }) : super(key: key);
 
   final data;
   final scrollDown;
   final scrollUp;
+  final setUserContent;
   @override
   State<Home> createState() => _HomeState();
 }
@@ -189,21 +244,49 @@ class _HomeState extends State<Home> {
             SizedBox(
               width : MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.width,
-              child : Image.network(widget.data[i]['image'])
+              child : widget.data[i]['image'].runtimeType == String
+                ? Image.network(widget.data[i]['image']) 
+                : Image.file(widget.data[i]['image'])
             ),
             DefaultTextStyle(
+              style : TextStyle(color : Colors.black),
               child: Container(
                 alignment: Alignment(-1.0, 1.0),
                 padding: EdgeInsets.fromLTRB(15, 15, 15, 15),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    GestureDetector(
+                      child : Text(
+                        widget.data[i]['user'], 
+                        style: TextStyle(fontWeight: FontWeight.bold)
+                      ),
+                      onTap : (){
+                        Navigator.push(context, 
+                          PageRouteBuilder(
+                            pageBuilder: (c, a1, a2) => Profile(),
+                            transitionsBuilder: (c, a1, a2, child) => 
+                              // FadeTransition(opacity : a1, child : child),
+                              SlideTransition(
+                                position: Tween(
+                                  begin: Offset(1.0, 0.0),
+                                  end : Offset(0.0, 0.0),
+                                ).animate(a1),
+                                child : child 
+                              )
+                            // transitionDuration: Duration(microseconds: 500)  
+                            
+
+                          )
+                        );
+                      }
+
+                    ),
                     Text("좋아요${widget.data[i]['likes']}", style: TextStyle(fontWeight: FontWeight.bold),),
-                    Text(widget.data[i]['user']),
+                    Text(widget.data[i]['date']),
                     Text(widget.data[i]['content'])],
                 ), 
               ),
-              style : TextStyle(color : Colors.black)
             ),
           ],
         )
@@ -219,16 +302,51 @@ class _HomeState extends State<Home> {
 
 
 class Upload extends StatelessWidget{
-  const Upload({Key? key}):super(key : key);
+  const Upload({Key? key, this.userImage, this.data, this.setUserContent, this.addmydata}):super(key : key);
+  final userImage;
+  final data;
+  final setUserContent;
+  final addmydata;
 
   @override 
   Widget build(BuildContext context){
     return Scaffold(
-      appBar : AppBar(),
+      appBar : AppBar(
+        actions : [
+          IconButton(
+            icon: Icon(Icons.add_box_outlined),
+            onPressed: () async{
+              
+              // var post = {};
+              // post['id'] = widget.data.length;
+              // post['image'] = widget.userImage;
+              // post['likes'] = 0;
+              // post['date'] = "November 10";
+              // post['content'] = "안녕";
+              // post['liked'] = false;
+              // post['user'] = 'sangyeob';
+              // setState(()=>{
+              //   widget.data.add(post)
+              // });
+              print(userImage);
+              addmydata();
+              Navigator.pop(context);
+              
+            },
+            iconSize: 30
+          )
+        ]
+      ),
       body : Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children : [
+          Image.file(userImage),
           Text("이미지 업로드 화면"),
+          TextField(
+            onChanged: (text){
+              setUserContent(text);
+            },
+          ),
           IconButton(
             onPressed : (){
               Navigator.pop(context);
@@ -236,9 +354,11 @@ class Upload extends StatelessWidget{
             icon : Icon(Icons.close)
           )
         ],
-        )
+      ),
+      
+       
     );
-  }
+  }  
 }
 
 // ignore: must_be_immutable
@@ -295,3 +415,58 @@ class Board extends StatelessWidget {
       //   child: Container()
       // )
       // Text("안녕", style : Theme.of(context).textTheme.bodyText2), //이렇게 하면 theme을 알아서 찾아서 그중에 원하는 style을 사용할 수 있음. 미리 만들어놓고 사용 가능!
+
+
+class StateStore extends ChangeNotifier{
+  var name = 'john kim';
+  var followers = 0;
+  var followed = false;
+
+  changeName(){
+    name = 'john park';
+    notifyListeners();
+  }
+  follow(){
+    if(!followed){
+      followers = followers + 1;
+      followed = true;
+    }
+    else{
+      followers = followers - 1;
+      followed = false;
+    } 
+
+    notifyListeners();
+  }
+
+}
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title : Text(context.watch<StateStore>().name)
+      ),
+      body : Column(
+        children: [
+          ElevatedButton(
+            onPressed: (){
+              context.read<StateStore>().changeName();
+            }, 
+            child: Text('버튼')
+          ),
+          Text('팔로워 ${context.watch<StateStore>().followers} 명'),
+          ElevatedButton(
+            onPressed: (){
+              context.read<StateStore>().follow();
+            }, 
+            child: context.watch<StateStore>().followed ? Text('언팔로우') : Text('팔로우')
+          )
+        ],
+      )
+    );
+  }
+}
